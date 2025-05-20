028cb8d68493afee1ce6b367219ea21566c1a2fa
Giacomo92 <info@giacomofabbian.it>
2025-05-20 21:53:50 +0200

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
-// next.config.js
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
     output: 'export',
-    basePath: isProd ? '/odps-validator' : '',
-    assetPrefix: isProd ? '/odps-validator/' : '',
-    trailingSlash: true
+    trailingSlash: true,
 };
